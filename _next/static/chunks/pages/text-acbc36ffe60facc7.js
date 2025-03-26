073dddd9faa42b29e8828c8da7bf42631b89c732
Your Name <you@example.com>
2025-03-26 13:27:21 +0800

    init

---
+++ _next/static/chunks/pages/text-acbc36ffe60facc7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2309],{64819:(_,e,n)=>{"use strict";n.r(e),n.d(e,{default:()=>u});var t=n(37876);function u(){return(0,t.jsx)("div",{children:"text..."})}},75205:(_,e,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/text",function(){return n(64819)}])}},_=>{var e=e=>_(_.s=e);_.O(0,[636,6593,8792],()=>e(75205)),_N_E=_.O()}]);
